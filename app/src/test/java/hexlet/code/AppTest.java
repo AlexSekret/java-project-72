d6c5ad81e896dea18b443aaa846375a8066ceb2d
AlexSekret <alexsekret3@gmail.com>
2025-01-13 20:50:15 +0300

    try fix jacoco

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
